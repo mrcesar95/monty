5328f0c0e6c227ddf6099b0bf2e1d49cba4c7c9b
Cesar Molina <cesaruxfxtx@gmail.com>
2022-01-13 13:56:40 -0500

    Adding op_selector.c

---
+++ op_selector.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * op_selector - selects function of opcode related to the line of monty.
+ * @l: number of line in the monty code.
+ * @top: pointer of the top of the stack.
+ *
+ * Return: pointer to functions related to command.
+ */
+void (*op_selector(int l, stack_t **top))(stack_t **top, unsigned int line_n)
+{
+	int i = 0;
+
+	instruction_t list[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{"sub", op_sub},
+		{"div", op_div},
+		{"mul", op_mul},
+		{"mod", op_mod},
+		{"pchar", op_pchar},
+		{"pstr", op_pstr},
+		{"rotl", op_rotl},
+		{NULL, NULL}
+	};
+
+	for (i = 0; list[i].opcode; i++)
+	{
+		if (strcmp(global_var.command_array[0], list[i].opcode) == SAME)
+		{
+			return (list[i].f);
+		}
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n",
+					l, global_var.command_array[0]);
+	free_stack(top);
+	fclose(global_var.fp);
+	exit(EXIT_FAILURE);
+}
